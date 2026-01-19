19a87e71b88465c1186222059a90bd768b295fbb
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2026-01-19 01:35:04 -0300

    chore: importar a função map do RxJS no serviço de paciente

--- src/app/services/paciente.ts
+++ src/app/services/paciente.ts
@@ -1,7 +1,7 @@
 // src/app/services/paciente.service.ts
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { PacienteDTO } from '../models/paciente.model';
 
